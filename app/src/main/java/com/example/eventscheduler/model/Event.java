6b469290360742ae791a9c29588a67c40e38264c
sajandangal <42007423+sajandangal@users.noreply.github.com>
2019-12-17 19:11:59 +0545

    Event Model Added

---
+++ app/src/main/java/com/example/eventscheduler/model/Event.java
@@ -0,0 +1,18 @@
+package com.example.eventscheduler.model;
+
+public class Event {
+
+    private String name;
+
+    public Event(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
