605dc4e5203b1b0080663d1fc3e003e0d19683e2
sajandangal <42007423+sajandangal@users.noreply.github.com>
2019-12-28 17:20:15 +0545

    EVENT api created

--- app/src/main/java/com/example/eventscheduler/activity/DashboardActivity.java
+++ app/src/main/java/com/example/eventscheduler/activity/DashboardActivity.java
@@ -1,33 +1,49 @@
 package com.example.eventscheduler.activity;
 
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.eventscheduler.R;
+
 import com.example.eventscheduler.api.UsersAPI;
 import com.example.eventscheduler.model.User;
+import com.example.eventscheduler.strictmode.StrictModeClass;
 import com.example.eventscheduler.url.Url;
 import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import static com.example.eventscheduler.url.Url.getInstance;
+import static com.example.eventscheduler.url.Url.token;
+
 public class DashboardActivity extends AppCompatActivity {
 
-    ImageView imgProgileImg;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_dashboard);
 
-        imgProgileImg = findViewById(R.id.imgProgileImg);
+
+
+
 
         findViewById(R.id.btnAdd).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,40 +52,52 @@ public class DashboardActivity extends AppCompatActivity {
             }
         });
 
-        loadCurrentUser();
-    }
-
-    private void loadCurrentUser() {
-
-        UsersAPI usersAPI = Url.getInstance().create(UsersAPI.class);
-        Call<User> userCall = usersAPI.getUserDetails(Url.token);
-
-        userCall.enqueue(new Callback<User>() {
+        findViewById(R.id.btnView).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onResponse(Call<User> call, Response<User> response) {
-                if (!response.isSuccessful()) {
-                    Toast.makeText(DashboardActivity.this, "Code " + response.code(), Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                String imgPath = Url.imagePath +  response.body().getImage();
-
-                Picasso.get().load(imgPath).into(imgProgileImg);
-
-
-//                StrictModeClass.StrictMode();
-//                try {
-//                    URL url = new URL(imgPath);
-//                    imgProgileImg.setImageBitmap(BitmapFactory.decodeStream((InputStream) url.getContent()));
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-            }
-
-            @Override
-            public void onFailure(Call<User> call, Throwable t) {
-
-                Toast.makeText(DashboardActivity.this, "Error " + t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                startActivity(new Intent(DashboardActivity.this, ViewEventActivity.class));
             }
         });
+
+        findViewById(R.id.btnLogout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(token!="Bearer "){
+                    token="Bearer ";
+
+                }
+
+                    Intent intent = new Intent(DashboardActivity.this,SplashActivity.class);
+
+                    startActivity(intent);
+                    finish();
+            }
+        });
+
+        findViewById(R.id.btnUserprofile).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(DashboardActivity.this,UserProfileActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+
+
+
     }
+
+
+
+    //here is rough code....
+
+
+
+//    public void profile(View view){
+//        Intent intent=new Intent(DashboardActivity.this,UserProfileActivity.class);
+//        startActivity(intent);
+//    }
+
+
 }
--- app/src/main/java/com/example/eventscheduler/api/EventAPI.java
+++ app/src/main/java/com/example/eventscheduler/api/EventAPI.java
@@ -1,15 +1,23 @@
 package com.example.eventscheduler.api;
 
+import com.example.eventscheduler.model.Event;
+
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
 
 public interface EventAPI {
 
     @FormUrlEncoded
-    @POST("tasks")
-    Call<Void> addTask(@Header("Authorization") String token, @Field("name") String task);
+    @POST("events")
+    Call<Void> addEvent(@Header("Authorization")String token, @Field("name") String event);
+
+    @GET("events")
+    Call<List<Event>> getEvent(@Header("Authorization")String token);
 
 }
