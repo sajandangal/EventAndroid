efcdb201f2b843d4c7a60bd702b65004e119573b
sajandangal <42007423+sajandangal@users.noreply.github.com>
2020-01-22 17:32:32 +0545

    Category API done

---
+++ app/src/main/java/com/example/eventscheduler/api/CategoryApi.java
@@ -0,0 +1,21 @@
+package com.example.eventscheduler.api;
+
+import com.example.eventscheduler.model.Category;
+import com.example.eventscheduler.model.Event;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+
+public interface CategoryApi {
+
+
+    @GET("categories")
+    Call<List<Category>> getCategory(@Header("Authorization")String token);
+
+}
